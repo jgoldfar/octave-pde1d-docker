7106295475cce3112d31529956a0a0e4f206e45c
Jonathan Goldfarb <jgoldfar@gmail.com>
2019-03-15 07:35:01 -0400

    Build GUI on this branch

--- build.sh
+++ build.sh
@@ -15,15 +15,15 @@ IMG_TARGET=builder
 
 
 # Build main entrypoint
-IMG_TARGET=octave
-docker build -f Dockerfile.debian --target=${IMG_TARGET} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+#IMG_TARGET=octave
+#docker build -f Dockerfile.debian --target=${IMG_TARGET} -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
+#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
 
 
 # Same as above, with GUI
-#IMG_TAG=pdepe-gui
-#docker build -f Dockerfile.gui -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
-#docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
+IMG_TAG=pdepe-gui
+docker build -f Dockerfile.gui -t ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG} .
+docker push ${DOCKER_USERNAME}/${DOCKER_REPO_BASE}:${IMG_TAG}
 
 # Alpine + Octave + PDE1D
 # Use Docker Hub's infrastructure
